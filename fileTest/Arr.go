199eb08079dc869d74e6288d29a80b1ca241ab10
Aria Lent <kaqyword2304@gmail.com>
2024-05-17 00:21:44 +0700

    banyak berubah

--- fileTest/Arr.go
+++ fileTest/Arr.go
@@ -2,15 +2,31 @@ package fileTest
 
 import "fmt"
 
-type data struct {
-	nama string
+type Data struct {
+	nama,asal, tanggalLahir, golongan_riwayat_penyakit string
 	umur int
 }
 
-type Arr [100]data
+type Arr [100]Data
 
 func InputData(arr *Arr, n int) {
-	fmt.Scan(&arr[n].nama, &arr[n].umur)
+	for i := 0; i < n; i++ {
+		fmt.Println("===================")
+		fmt.Printf("    Pasien ke-%d   \n", i+1)
+		fmt.Println("===================")
+		fmt.Print("Masukan nama pasien: ")
+		fmt.Scan(&arr[i].nama)
+		fmt.Print("Masukan asal pasien: ")
+		fmt.Scan(&arr[i].asal)
+		fmt.Print("Masukan umur pasien: ")
+		fmt.Scan(&arr[i].umur)
+		fmt.Print("Masukan tanggal lahir pasien: ")
+		fmt.Scan(&arr[i].tanggalLahir)
+		fmt.Print("Masukan golongan riwayat penyakit pasien: ")
+		fmt.Scan(&arr[i].golongan_riwayat_penyakit)
+		fmt.Print("\n")
+		
+	}
 }
 var Datas Arr
 
@@ -19,7 +35,7 @@ func DeleteData(Arr *Arr, n *int, nama string) {
 	for i := 0; i < *n ||i >= len(Arr); i++ {
 		if Arr[i].nama == nama {
 			copy(Arr[i:], Arr[i+1:*n])
-			Arr[*n-1] = data{} // Menghapus data terakhir setelah geser
+			Arr[*n-1] = Data{} // Menghapus data terakhir setelah geser
 			*n--
 			test = true
 			break
@@ -30,19 +46,81 @@ func DeleteData(Arr *Arr, n *int, nama string) {
 	}
 }
 
-func EditData(Arr *Arr, n int, namaS string) {
-	var nama string
-	var umur int
-	for i := 0; i < n; i++ {
-		if Arr[i].nama==namaS {
-			fmt.Scan(&nama, &umur)
-			Arr[i].nama = nama
-			Arr[i].umur = umur
+func FindData(Arr *Arr, n int, namaS string, pas *bool) {
+	for i := 0; i < n && !*pas; i++ {
+		if Arr[i].nama == namaS {
+			fmt.Printf("%s %d %s %s %s", Arr[i].nama, Arr[i].umur, Arr[i].asal, Arr[i].tanggalLahir, Arr[i].golongan_riwayat_penyakit)
+			*pas = true
 		}
 	}
 }
+
+func EditData(arr *Arr, n int, namaS string, pas *bool) {
+	for i := 0; i < n && !*pas; i++ {
+		if arr[i].nama == namaS {
+			fmt.Print("Masukan nama pasien: ")
+			fmt.Scan(&arr[i].nama)
+			fmt.Print("Masukan asal pasien: ")
+			fmt.Scan(&arr[i].asal)
+			fmt.Print("Masukan umur pasien: ")
+			fmt.Scan(&arr[i].umur)
+			fmt.Print("Masukan tanggal lahir pasien: ")
+			fmt.Scan(&arr[i].tanggalLahir)
+			fmt.Print("Masukan golongan riwayat penyakit pasien: ")
+			fmt.Scan(&arr[i].golongan_riwayat_penyakit)
+			fmt.Print("\n")
+			*pas = true
+		}
+	}
+}
+
 func PrintData(arr Arr, n int) {
 	for i := 0; i < n; i++ {
-		fmt.Printf("Nama: %s Umur: %d\n", arr[i].nama, arr[i].umur)
+		fmt.Printf("%d. Nama: %s Umur: %d Asal: %s Tanggal Lahir: %s Gol-riwayat: %s\n",i+1, arr[i].nama, arr[i].umur, arr[i].asal, arr[i].tanggalLahir, arr[i].golongan_riwayat_penyakit)
+	}
+}
+
+func cetakAntrian() {
+
+}
+
+func Interface() {
+		fmt.Println("*************************************")
+		fmt.Println("*                                   *")
+		fmt.Println("*       ======================      *")
+		fmt.Println("*           Selamat Datang          *")
+		fmt.Println("*         Di Klinik Tong Fung       *")
+		fmt.Println("*       =======================     *")
+		fmt.Println("*       1. Pendaftaran Pasien       *")
+		fmt.Println("*       2. Mencari Pasien           *")
+		fmt.Println("*       3. Mengedit Data Pasien     *")
+		fmt.Println("*       4. Menghapus Data Pasien    *")
+		fmt.Println("*       5. Mengurutkan Pasien       *")
+		fmt.Println("*       ========================    *")
+		fmt.Println("*                                   *")
+		fmt.Println("*************************************")
+		fmt.Print("Silahkan Pilih Menu : ")
+}
+
+
+func HapusData(Arr *Arr, n int, k *int, nmas string) {
+	Find(Arr, n, k,nmas)
+	if *k!=-1 {
+		fmt.Println("Data ditemukan")
+		for i := *k; i < n-1; i++ {
+			Arr[i] = Arr[i+1]
+		}
+	}else {
+		fmt.Print("Data tidak di temukan")
+	}
+}
+
+func Find(Arr *Arr, n int, k *int, nmas string) {
+	*k = -1
+	for i := 0; i < n; i++ {
+		if Arr[i].nama==nmas {
+			*k = i
+			break
+		}
 	}
 }
--- main.go
+++ main.go
@@ -3,35 +3,51 @@ package main
 import (
 	"fmt"
 	"test-tubes/fileTest"
-	"test-tubes/interfaces"
 )
-
 func main() {
-	var nama string
-	// input data
-	interfaces.InputInter()
-	var n int
-	fmt.Scan(&n)
-	for i := 0; i < n; i++ {
-		// Dari interface
-		interfaces.InputPasien(i)
+	var aksi,banPasien int
+	var dataPasien fileTest.Arr
+	var namPasien string
+	var adaPasien bool
 
-		// Dari fileTest
-		fileTest.InputData(&fileTest.Datas, i)
+	for {
+		fileTest.Interface()
+		fmt.Scan(&aksi)
+		if aksi ==1 {
+			fmt.Print("Berapa banyak pasien yang ingin di tambahkan: ")
+			fmt.Scan(&banPasien)
+			fileTest.InputData(&dataPasien, banPasien)
+			fmt.Print("Data yang masukan telah terkirim ke sistem\n\n")
+		}else if aksi ==2 {
+			adaPasien = false
+			fmt.Print("Masukan Nama Pasien yang ingin dicari: ")
+			fmt.Scan(&namPasien)
+			fileTest.FindData(&dataPasien, banPasien, namPasien, &adaPasien)
+			if adaPasien {
+				fmt.Println("Data pasien ditemukan")
+			}else if !adaPasien && banPasien==0 {
+				fmt.Println("Data pasien tidak ada, silahkan masukan pasien")
+			}else {
+				fmt.Println("Data pasien tidak ada")
+			}
+		}else if aksi==3 {
+			adaPasien = false
+			fmt.Print("Masukan Nama Pasien yang ingin diubah: ")
+			fmt.Scan(&namPasien)
+			fileTest.EditData(&dataPasien, banPasien, namPasien, &adaPasien)
+			if adaPasien {
+				fmt.Print("Data berhasil di ubah\n")
+			}else {
+				fmt.Print("Data tidak ditemukan\n")
+			}
+		}else if aksi==4 {
+			var k int
+			fmt.Print("Masukan Nama Pasien yang ingin diubah: ")
+			fmt.Scan(&namPasien)
+		fileTest.HapusData(&dataPasien, banPasien, &k, namPasien)
+		}else {
+			fileTest.PrintData(dataPasien, banPasien)
+		}
 	}
 
-	// print data
-	fileTest.PrintData(fileTest.Datas, n)
-
-	// hapus data
-	interfaces.HapusData()
-	fmt.Scan(&nama)
-	fileTest.DeleteData(&fileTest.Datas, &n, nama)
-	fileTest.PrintData(fileTest.Datas, n)
-	
-	// edit data
-	interfaces.EditData()
-	fmt.Scan(&nama)
-	fileTest.EditData(&fileTest.Datas, n, nama)
-	fileTest.PrintData(fileTest.Datas, n)
 }
